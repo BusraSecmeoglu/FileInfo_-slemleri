48b277b5564a1f92c7625a30100bb457a364fe26
büşra seçmeoğlu <busra__secmeoglu@hotmail.com>
2018-12-13 23:42:49 +0300

    Add project files.

---
+++ FileInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FileInfo_İslemleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo NewFile = new FileInfo(@"C:\Test\HedefDizini\NewFilw.txt");
+            if (NewFile.Exists) //File sınıfı: if (File.Exist(@"C:\Test\NewFile.txt"))
+            {
+                //Dosya silme işlemi  //File sınıfı: File.Delete(@"C:\Test\NewFile.txt"))
+                NewFile.Delete();
+            }
+            //Dosya oluşturma
+            NewFile.CreateText();  // File sınıfı: File.CreateText(@"C:\Test\NewFilw.txt"))
+            Console.WriteLine("Oluşturma tarihi:" + NewFile.CreationTime);
+            Console.WriteLine("Son erişim  tarihi:" + NewFile.LastAccessTime);
+            Console.WriteLine("Son değiştirilme tarihi:" + NewFile.LastWriteTime);
+            Console.WriteLine("Bulunduğu dizinin adı:" + NewFile.DirectoryName);
+            Console.WriteLine("Dosyanın adı:" + NewFile.Name);
+            Console.WriteLine("Dosyanın tam adı:" + NewFile.FullName);
+            Console.WriteLine("Dosyanın uzantısı" + NewFile.Extension);
+            //Dosya kopyalama
+            NewFile.CopyTo(@"C:\Test\nt.txt");
+            Console.ReadKey();
+
+
+        }
+    }
+}
